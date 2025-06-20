5550c8196c3b5220dc8e34cb149f5138d9a0d4ac
saulompg <saulo.mpg@hotmail.com>
2025-06-19 22:57:18 -0300

    update arquivos

--- SistemaBiblioteca/Professor.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SistemaBiblioteca
-{
-    internal class Professor : Usuario
-    {
-        public Professor(string codigo, string nome) : base(codigo, nome, new EmprestimoProfessor())
-        {
-        }
-    }
-}
--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -1,5 +1,33 @@
-﻿using SistemaBiblioteca;
+﻿using ProjetoBiblioteca.comando;
 
-Repositorio rep = Repositorio.GetInstancia();
-Usuario usuario = rep.BuscarUsuarioPorCodigo("123");
-Console.WriteLine(usuario.Nome);
+bool executando = true;
+
+SistemaBiblioteca sistemaBiblioteca = new SistemaBiblioteca();
+
+sistemaBiblioteca.Adicionar("emp", args => new EmprestarComando(args[1], args[2]));
+sistemaBiblioteca.Adicionar("dev", args => new DevolverComando(args[1], args[2]));
+sistemaBiblioteca.Adicionar("res", args => new ReservarComando(args[1], args[2]));
+sistemaBiblioteca.Adicionar("obs", args => new ObservarComando(args[1], args[2]));
+sistemaBiblioteca.Adicionar("ntf", args => new ConsultarNotificacaoComando(args[1]));
+sistemaBiblioteca.Adicionar("usu", args => new ConsultarUsuarioComando(args[1]));
+sistemaBiblioteca.Adicionar("liv", args => new ConsultarLivroComando(args[1]));
+sistemaBiblioteca.Adicionar("sai", _ => new SairComando(() => executando = false));
+
+while (executando)
+{
+    Console.WriteLine("Digite um comando:");
+    Console.Write("> ");
+    var input = Console.ReadLine()?.Trim();
+    if (string.IsNullOrWhiteSpace(input))
+        continue;
+    try
+    {
+        sistemaBiblioteca.ExecutarComando(input);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Erro | " + e.Message);
+    }
+}
+
+Console.WriteLine("Programa finalizado.");
