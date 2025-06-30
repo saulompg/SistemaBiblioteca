7a20f76aea84d82176559338126aac459cf8cead
saulompg <saulo.mpg@hotmail.com>
2025-06-30 15:36:19 -0300

    validação de argumentos na factory

--- SistemaBiblioteca/factory/ReservarCommandFactory.cs
+++ SistemaBiblioteca/factory/ReservarCommandFactory.cs
@@ -6,6 +6,7 @@ internal class ReservarCommandFactory : ICommandFactory
 {
     public ICommand Criar(string[] args)
     {
+        if (args.Length < 3) throw new IndexOutOfRangeException("Os parâmetros passados para o comando não estão corretos");
         return new ReservarCommand(args[1], args[2]);
     }
 }
