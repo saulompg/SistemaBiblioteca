de298f6bd19d2c8372bb760bcbe4b092f109c129
saulompg <saulo.mpg@hotmail.com>
2025-07-01 21:49:26 -0300

    Implementacao interface UI

---
+++ SistemaBiblioteca/ConsoleUI.cs
@@ -0,0 +1,47 @@
+﻿using SistemaBiblioteca.command;
+
+namespace SistemaBiblioteca
+{
+    public class ConsoleUI
+    {
+        private readonly CommandInvoker _invoker;
+        private bool _executando = true;
+
+        public ConsoleUI(CommandInvoker invoker)
+        {
+            _invoker = invoker;
+        }
+
+        public void Iniciar()
+        {
+            Console.WriteLine(".: Sistema de Biblioteca Acadêmica :.");
+
+            while (_executando)
+            {
+                Console.WriteLine("Digite um comando:");
+                Console.Write("> ");
+                string? input = Console.ReadLine()?.Trim();
+
+                if (string.IsNullOrWhiteSpace(input))
+                    continue;
+
+                try
+                {
+                    _invoker.ExecutarComando(input, out string output);
+                    Console.WriteLine(output);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro | " + e.Message);
+                }
+            }
+
+            Console.WriteLine("Programa finalizado.");
+        }
+
+        public void Encerrar()
+        {
+            _executando = false;
+        }
+    }
+}
--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -1,28 +1,29 @@
-﻿using ProjetoBiblioteca;
-using ProjetoBiblioteca.command;
-using ProjetoBiblioteca.entidade;
-using ProjetoBiblioteca.factory;
-
-bool executando = true;
-
-CommandRegistry commandRegistry = new CommandRegistry();
-
-// preencher fabrica de comandos
-commandRegistry.AdicionarComando("emp", new EmprestarCommandFactory());
-commandRegistry.AdicionarComando("dev", new DevolverCommandFactory());
-commandRegistry.AdicionarComando("res", new ReservarCommandFactory());
-commandRegistry.AdicionarComando("obs", new ObservarCommandFactory());
-commandRegistry.AdicionarComando("ntf", new ConsultarNotificacaoCommandFactory());
-commandRegistry.AdicionarComando("usu", new ConsultarUsuarioCommandFactory());
-commandRegistry.AdicionarComando("liv", new ConsultarLivroCommandFactory());
-commandRegistry.AdicionarComando("sai", new SairCommandFactory(() => executando = false));
+﻿using SistemaBiblioteca;
+using SistemaBiblioteca.command;
+using SistemaBiblioteca.entidade;
+using SistemaBiblioteca.factory;
 
 Repository repo = Repository.Instancia;
+CommandRegistry registry = new CommandRegistry();
+CommandInvoker invoker = new CommandInvoker(registry);
+ConsoleUI consoleUI = new ConsoleUI(invoker);
+
+// preencher fabrica de comandos
+registry.AdicionarComando("emp", new EmprestarCommandFactory());
+registry.AdicionarComando("dev", new DevolverCommandFactory());
+registry.AdicionarComando("res", new ReservarCommandFactory());
+registry.AdicionarComando("obs", new ObservarCommandFactory());
+registry.AdicionarComando("ntf", new ConsultarNotificacaoCommandFactory());
+registry.AdicionarComando("usu", new ConsultarUsuarioCommandFactory());
+registry.AdicionarComando("liv", new ConsultarLivroCommandFactory());
+registry.AdicionarComando("sai", new SairCommandFactory(() => consoleUI.Encerrar()));
+
 // preencher repositório usuário
 repo.AdicionarUsuario(new AlunoGraduacao("123", "João da Silva"));
 repo.AdicionarUsuario(new AlunoPosGraduacao("456", "Luiz Fernando Rodrigues"));
 repo.AdicionarUsuario(new AlunoGraduacao("789", "Pedro Paulo"));
 repo.AdicionarUsuario(new Professor("100", "Carlos Lucena"));
+
 // preencher repositório livro
 repo.AdicionarLivro(new Livro("100", "Engenharia de Software", "Addison Wesley", ["Ian Sommervile"], "6a", 2000));
 repo.AdicionarLivro(new Livro("101", "UML - Guia do Usuário", "Campus", ["Grady Booch", "James Rumbaugh", "Ivar Jacobson"], "7a", 2000));
@@ -32,6 +33,7 @@ repo.AdicionarLivro(new Livro("300", "Refactoring: Improving the Design of Exist
 repo.AdicionarLivro(new Livro("301", "Software Metrics: A Rigorous and Practical Approach", "CRC Press", ["Norman Fenton", "James Bieman"], "3a", 2014));
 repo.AdicionarLivro(new Livro("400", "Design Patterns: Element of Reusable Object-Oriented Software", "Addison Wesley Professional", ["Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides"], "1a", 1994));
 repo.AdicionarLivro(new Livro("401", "UML Distilled: A Brief Guide to the Standard Object Modeling Language", "Addison Wesley Professional", ["Martin Fowler"], "3a", 2003));
+
 // adicionando exemplares
 repo.BuscarLivroPorCodigo("100").AdicionarExemplar("01");
 repo.BuscarLivroPorCodigo("100").AdicionarExemplar("02");
@@ -44,25 +46,4 @@ repo.BuscarLivroPorCodigo("400").AdicionarExemplar("08");
 repo.BuscarLivroPorCodigo("400").AdicionarExemplar("09");
 
 // execução principal
-CommandInvoker invoker = new CommandInvoker(commandRegistry);
-
-Console.WriteLine("Sistema de Biblioteca Acadêmica Iniciado");
-while (executando)
-{
-    Console.WriteLine("Digite um comando:");
-    Console.Write("> ");
-    string? input = Console.ReadLine()?.Trim();
-    if (string.IsNullOrWhiteSpace(input))
-        continue;
-    try
-    {
-        invoker.ExecutarComando(input, out string output);
-        Console.WriteLine(output);
-    }
-    catch (Exception e)
-    {
-        Console.WriteLine("Erro | " + e.Message);
-    }
-}
-
-Console.WriteLine("Programa finalizado.");
+consoleUI.Iniciar();
