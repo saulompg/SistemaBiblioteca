39ec656bc2de8ffcdd528ff1164ad69eecbcb819
saulompg <saulo.mpg@hotmail.com>
2025-07-03 12:27:45 -0300

    update ui

--- SistemaBiblioteca/Program.cs
+++ SistemaBiblioteca/Program.cs
@@ -16,7 +16,7 @@ registry.AdicionarComando("obs", new ObservarCommandFactory());
 registry.AdicionarComando("ntf", new ConsultarNotificacaoCommandFactory());
 registry.AdicionarComando("usu", new ConsultarUsuarioCommandFactory());
 registry.AdicionarComando("liv", new ConsultarLivroCommandFactory());
-registry.AdicionarComando("sai", new SairCommandFactory(() => consoleUI.Encerrar()));
+registry.AdicionarComando("sai", new SairCommandFactory(consoleUI.Encerrar));
 
 // preencher repositório usuário
 repo.AdicionarUsuario(new AlunoGraduacao("123", "João da Silva"));
@@ -27,9 +27,9 @@ repo.AdicionarUsuario(new Professor("100", "Carlos Lucena"));
 // preencher repositório livro
 repo.AdicionarLivro(new Livro("100", "Engenharia de Software", "Addison Wesley", ["Ian Sommervile"], "6a", 2000));
 repo.AdicionarLivro(new Livro("101", "UML - Guia do Usuário", "Campus", ["Grady Booch", "James Rumbaugh", "Ivar Jacobson"], "7a", 2000));
-repo.AdicionarLivro(new Livro("200", "Code Complete", "Microsoft Press",["Steve McConnell"], "2a", 2014));
+repo.AdicionarLivro(new Livro("200", "Code Complete", "Microsoft Press", ["Steve McConnell"], "2a", 2014));
 repo.AdicionarLivro(new Livro("201", "Agile Software Development, Principles, Patterns and Practices", "Microsoft Press", ["Robert Martin"], "1a", 2002));
-repo.AdicionarLivro(new Livro("300", "Refactoring: Improving the Design of Existing Code", "Addison Wesley Professional",["Martin Fowler"], "1a", 1999));
+repo.AdicionarLivro(new Livro("300", "Refactoring: Improving the Design of Existing Code", "Addison Wesley Professional", ["Martin Fowler"], "1a", 1999));
 repo.AdicionarLivro(new Livro("301", "Software Metrics: A Rigorous and Practical Approach", "CRC Press", ["Norman Fenton", "James Bieman"], "3a", 2014));
 repo.AdicionarLivro(new Livro("400", "Design Patterns: Element of Reusable Object-Oriented Software", "Addison Wesley Professional", ["Erich Gamma", "Richard Helm", "Ralph Johnson", "John Vlissides"], "1a", 1994));
 repo.AdicionarLivro(new Livro("401", "UML Distilled: A Brief Guide to the Standard Object Modeling Language", "Addison Wesley Professional", ["Martin Fowler"], "3a", 2003));
